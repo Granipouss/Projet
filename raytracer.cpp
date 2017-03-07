c720765b6aa5b6e35004d0ac1aa5dc31a9b711b7
Brendan <Granipouss@gmail.com>
2017-03-07 13:02:07 +0100

    include last line

--- raytracer.cpp
+++ raytracer.cpp
@@ -39,22 +39,19 @@
   #define INFINITY 1e8
 #endif
 
-template<typename T>
-class Vec3
-{
-public:
+template<typename T> class Vec3 {
+  public:
     T x, y, z;
     Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
     Vec3(T xx) : x(xx), y(xx), z(xx) {}
     Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
-    Vec3& normalize()
-    {
-        T nor2 = length2();
-        if (nor2 > 0) {
-            T invNor = 1 / sqrt(nor2);
-            x *= invNor, y *= invNor, z *= invNor;
-        }
-        return *this;
+    Vec3& normalize () {
+      T nor2 = length2();
+      if (nor2 > 0) {
+        T invNor = 1 / sqrt(nor2);
+        x *= invNor, y *= invNor, z *= invNor;
+      }
+      return *this;
     }
     Vec3<T> operator * (const T &f) const { return Vec3<T>(x * f, y * f, z * f); }
     Vec3<T> operator * (const Vec3<T> &v) const { return Vec3<T>(x * v.x, y * v.y, z * v.z); }
@@ -66,47 +63,45 @@ public:
     Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }
     T length2() const { return x * x + y * y + z * z; }
     T length() const { return sqrt(length2()); }
-    friend std::ostream & operator << (std::ostream &os, const Vec3<T> &v)
-    {
-        os << "[" << v.x << " " << v.y << " " << v.z << "]";
-        return os;
+    friend std::ostream & operator << (std::ostream &os, const Vec3<T> &v) {
+      os << "[" << v.x << " " << v.y << " " << v.z << "]";
+      return os;
     }
 };
 
 typedef Vec3<float> Vec3f;
 
-class Sphere
-{
-public:
+class Sphere {
+  public:
     Vec3f center;                           /// position of the sphere
     float radius, radius2;                  /// sphere radius and radius^2
     Vec3f surfaceColor, emissionColor;      /// surface color and emission (light)
     float transparency, reflection;         /// surface transparency and reflectivity
-    Sphere(
-        const Vec3f &c,
-        const float &r,
-        const Vec3f &sc,
-        const float &refl = 0,
-        const float &transp = 0,
-        const Vec3f &ec = 0) :
-        center(c), radius(r), radius2(r * r), surfaceColor(sc), emissionColor(ec),
-        transparency(transp), reflection(refl)
+    Sphere (
+      const Vec3f &c,
+      const float &r,
+      const Vec3f &sc,
+      const float &refl = 0,
+      const float &transp = 0,
+      const Vec3f &ec = 0) :
+      center(c), radius(r), radius2(r * r), surfaceColor(sc), emissionColor(ec),
+      transparency(transp), reflection(refl)
     { /* empty */ }
+
     //[comment]
     // Compute a ray-sphere intersection using the geometric solution
     //[/comment]
-    bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1) const
-    {
-        Vec3f l = center - rayorig;
-        float tca = l.dot(raydir);
-        if (tca < 0) return false;
-        float d2 = l.dot(l) - tca * tca;
-        if (d2 > radius2) return false;
-        float thc = sqrt(radius2 - d2);
-        t0 = tca - thc;
-        t1 = tca + thc;
-        
-        return true;
+    bool intersect (const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1) const {
+      Vec3f l = center - rayorig;
+      float tca = l.dot(raydir);
+      if (tca < 0) return false;
+      float d2 = l.dot(l) - tca * tca;
+      if (d2 > radius2) return false;
+      float thc = sqrt(radius2 - d2);
+      t0 = tca - thc;
+      t1 = tca + thc;
+
+      return true;
     }
 };
 
@@ -115,9 +110,8 @@ public:
 //[/comment]
 #define MAX_RAY_DEPTH 5
 
-float mix(const float &a, const float &b, const float &mix)
-{
-    return b * mix + a * (1 - mix);
+float mix (const float &a, const float &b, const float &mix) {
+  return b * mix + a * (1 - mix);
 }
 
 //[comment]
@@ -130,87 +124,88 @@ float mix(const float &a, const float &b, const float &mix)
 // is the color of the object at the intersection point, otherwise it returns
 // the background color.
 //[/comment]
-Vec3f trace(
-    const Vec3f &rayorig,
-    const Vec3f &raydir,
-    const std::vector<Sphere> &spheres,
-    const int &depth)
-{
-    //if (raydir.length() != 1) std::cerr << "Error " << raydir << std::endl;
-    float tnear = INFINITY;
-    const Sphere* sphere = NULL;
-    // find intersection of this ray with the sphere in the scene
+Vec3f trace (
+  const Vec3f &rayorig,
+  const Vec3f &raydir,
+  const std::vector<Sphere> &spheres,
+  const int &depth
+) {
+  //if (raydir.length() != 1) std::cerr << "Error " << raydir << std::endl;
+  float tnear = INFINITY;
+  const Sphere* sphere = NULL;
+  // find intersection of this ray with the sphere in the scene
+  for (unsigned i = 0; i < spheres.size(); ++i) {
+    float t0 = INFINITY, t1 = INFINITY;
+    if (spheres[i].intersect(rayorig, raydir, t0, t1)) {
+      if (t0 < 0) t0 = t1;
+      if (t0 < tnear) {
+        tnear = t0;
+        sphere = &spheres[i];
+      }
+    }
+  }
+  // if there's no intersection return black or background color
+  if (!sphere) return Vec3f(2);
+  Vec3f surfaceColor = 0; // color of the ray/surfaceof the object intersected by the ray
+  Vec3f phit = rayorig + raydir * tnear; // point of intersection
+  Vec3f nhit = phit - sphere->center; // normal at the intersection point
+  nhit.normalize(); // normalize normal direction
+  // If the normal and the view direction are not opposite to each other
+  // reverse the normal direction. That also means we are inside the sphere so set
+  // the inside bool to true. Finally reverse the sign of IdotN which we want
+  // positive.
+  float bias = 1e-4; // add some bias to the point from which we will be tracing
+  bool inside = false;
+  if (raydir.dot(nhit) > 0) nhit = -nhit, inside = true;
+  if ((sphere->transparency > 0 || sphere->reflection > 0) && depth < MAX_RAY_DEPTH) {
+    float facingratio = -raydir.dot(nhit);
+    // change the mix value to tweak the effect
+    float fresneleffect = mix(pow(1 - facingratio, 3), 1, 0.1);
+    // compute reflection direction (not need to normalize because all vectors
+    // are already normalized)
+    Vec3f refldir = raydir - nhit * 2 * raydir.dot(nhit);
+    refldir.normalize();
+    Vec3f reflection = trace(phit + nhit * bias, refldir, spheres, depth + 1);
+    Vec3f refraction = 0;
+    // if the sphere is also transparent compute refraction ray (transmission)
+    if (sphere->transparency) {
+      float ior = 1.1, eta = (inside) ? ior : 1 / ior; // are we inside or outside the surface?
+      float cosi = -nhit.dot(raydir);
+      float k = 1 - eta * eta * (1 - cosi * cosi);
+      Vec3f refrdir = raydir * eta + nhit * (eta *  cosi - sqrt(k));
+      refrdir.normalize();
+      refraction = trace(phit - nhit * bias, refrdir, spheres, depth + 1);
+    }
+    // the result is a mix of reflection and refraction (if the sphere is transparent)
+    surfaceColor = (
+      reflection * fresneleffect +
+      refraction * (1 - fresneleffect) * sphere->transparency
+    ) * sphere->surfaceColor;
+  }
+  else {
+    // it's a diffuse object, no need to raytrace any further
     for (unsigned i = 0; i < spheres.size(); ++i) {
-        float t0 = INFINITY, t1 = INFINITY;
-        if (spheres[i].intersect(rayorig, raydir, t0, t1)) {
-            if (t0 < 0) t0 = t1;
-            if (t0 < tnear) {
-                tnear = t0;
-                sphere = &spheres[i];
+      if (spheres[i].emissionColor.x > 0) {
+        // this is a light
+        Vec3f transmission = 1;
+        Vec3f lightDirection = spheres[i].center - phit;
+        lightDirection.normalize();
+        for (unsigned j = 0; j < spheres.size(); ++j) {
+          if (i != j) {
+            float t0, t1;
+            if (spheres[j].intersect(phit + nhit * bias, lightDirection, t0, t1)) {
+              transmission = 0;
+              break;
             }
+          }
         }
+        surfaceColor += sphere->surfaceColor * transmission *
+        std::max(float(0), nhit.dot(lightDirection)) * spheres[i].emissionColor;
+      }
     }
-    // if there's no intersection return black or background color
-    if (!sphere) return Vec3f(2);
-    Vec3f surfaceColor = 0; // color of the ray/surfaceof the object intersected by the ray
-    Vec3f phit = rayorig + raydir * tnear; // point of intersection
-    Vec3f nhit = phit - sphere->center; // normal at the intersection point
-    nhit.normalize(); // normalize normal direction
-    // If the normal and the view direction are not opposite to each other
-    // reverse the normal direction. That also means we are inside the sphere so set
-    // the inside bool to true. Finally reverse the sign of IdotN which we want
-    // positive.
-    float bias = 1e-4; // add some bias to the point from which we will be tracing
-    bool inside = false;
-    if (raydir.dot(nhit) > 0) nhit = -nhit, inside = true;
-    if ((sphere->transparency > 0 || sphere->reflection > 0) && depth < MAX_RAY_DEPTH) {
-        float facingratio = -raydir.dot(nhit);
-        // change the mix value to tweak the effect
-        float fresneleffect = mix(pow(1 - facingratio, 3), 1, 0.1);
-        // compute reflection direction (not need to normalize because all vectors
-        // are already normalized)
-        Vec3f refldir = raydir - nhit * 2 * raydir.dot(nhit);
-        refldir.normalize();
-        Vec3f reflection = trace(phit + nhit * bias, refldir, spheres, depth + 1);
-        Vec3f refraction = 0;
-        // if the sphere is also transparent compute refraction ray (transmission)
-        if (sphere->transparency) {
-            float ior = 1.1, eta = (inside) ? ior : 1 / ior; // are we inside or outside the surface?
-            float cosi = -nhit.dot(raydir);
-            float k = 1 - eta * eta * (1 - cosi * cosi);
-            Vec3f refrdir = raydir * eta + nhit * (eta *  cosi - sqrt(k));
-            refrdir.normalize();
-            refraction = trace(phit - nhit * bias, refrdir, spheres, depth + 1);
-        }
-        // the result is a mix of reflection and refraction (if the sphere is transparent)
-        surfaceColor = (
-            reflection * fresneleffect +
-            refraction * (1 - fresneleffect) * sphere->transparency) * sphere->surfaceColor;
-    }
-    else {
-        // it's a diffuse object, no need to raytrace any further
-        for (unsigned i = 0; i < spheres.size(); ++i) {
-            if (spheres[i].emissionColor.x > 0) {
-                // this is a light
-                Vec3f transmission = 1;
-                Vec3f lightDirection = spheres[i].center - phit;
-                lightDirection.normalize();
-                for (unsigned j = 0; j < spheres.size(); ++j) {
-                    if (i != j) {
-                        float t0, t1;
-                        if (spheres[j].intersect(phit + nhit * bias, lightDirection, t0, t1)) {
-                            transmission = 0;
-                            break;
-                        }
-                    }
-                }
-                surfaceColor += sphere->surfaceColor * transmission *
-                std::max(float(0), nhit.dot(lightDirection)) * spheres[i].emissionColor;
-            }
-        }
-    }
-    
-    return surfaceColor + sphere->emissionColor;
+  }
+
+  return surfaceColor + sphere->emissionColor;
 }
 
 void saveImage (unsigned width, unsigned height, Vec3f* image) {
@@ -238,9 +233,8 @@ void render (
   Vec3f* pixel,
   const std::vector<Sphere> &spheres
  ) {
-	
   volatile std::atomic_int num_line(start - 1);
-	
+
   unsigned num_cpus = std::thread::hardware_concurrency();
   std::vector<std::thread> threads;
   auto iterate = [width, height, end, &num_line, &pixel, &spheres] () {
@@ -259,13 +253,14 @@ void render (
 				raydir.normalize();
 				pixel[x + width * y] = trace(Vec3f(0), raydir, spheres, 0);
 			}
-		} 
+		}
 	};
-									
+
   for (unsigned c = 0; c < num_cpus-1; ++c) {
     threads.push_back( std::thread(iterate) );
   }
   iterate();
+
   for (auto& t : threads) t.join();
 }
 
@@ -295,24 +290,24 @@ int main (int nargs, char* argv[]) {
 	// light
 	spheres.push_back(Sphere(Vec3f( 0.0,     20, -30),     3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3)));
 	// render(spheres);
-	
+
   int provided;
 	MPI_Init_thread(&nargs, &argv, MPI_THREAD_MULTIPLE, &provided);
-  
+
   // Start MPI
   // MPI_Init(&nargs, &argv);
   int rank, nbp;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &nbp);
   MPI_Status status;
-  
+
   int master = 0;
   int endMsg = -1;
   int bandHeight = 16;
 	unsigned width = 1280, height = 1024;
 	unsigned size = width * height* 3;
   // Vec3f *image = new Vec3f[width * height], *pixel = image;
-  
+
   if (rank == master) {
     printf("I'm the master\n");
     // Init
@@ -325,7 +320,7 @@ int main (int nargs, char* argv[]) {
 	  Vec3f *image = new Vec3f[width * height], *pixel = image;
 	  // Loop
 	  int nbTask = (int) height / bandHeight;
-	  while (countTask < nbTask) {
+	  while (countTask <= nbTask) {
 	    Vec3f *partial = new Vec3f[width * height];
       MPI_Recv(partial, size, MPI_FLOAT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
       for (unsigned i = 0; i < width * height; ++i) pixel[i] += partial[i];
@@ -356,10 +351,9 @@ int main (int nargs, char* argv[]) {
       }
     }
   }
-  	
+
   // End MPI
   MPI_Finalize();
-  
-  
+
 	return 0;
 }
